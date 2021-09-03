9c136aedf035ea8401cfbc22c8788dcf98a15749
tmdgh1592 <tmdgh1592@naver.com>
2021-09-04 01:40:46 +0900

    - 프로필 저장했을 때 Firebase DB에 Device Local 경로 저장해서 프로필 못불러오던 부분 수정

--- app/src/main/java/com/app/buna/sharingmarket/adapter/ChatRecyclerAdatper.kt
+++ app/src/main/java/com/app/buna/sharingmarket/adapter/ChatRecyclerAdatper.kt
@@ -2,35 +2,38 @@ package com.app.buna.sharingmarket.adapter
 
 import android.net.Uri
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.constraintlayout.widget.ConstraintSet
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 import com.app.buna.sharingmarket.R
-import com.app.buna.sharingmarket.databinding.ChatItemLayoutBinding
+import com.app.buna.sharingmarket.databinding.ChatMeItemLayoutBinding
+import com.app.buna.sharingmarket.databinding.ChatOtherItemLayoutBinding
 import com.app.buna.sharingmarket.model.items.chat.ChatModel
 import com.app.buna.sharingmarket.model.items.chat.UserModel
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
-class ChatRecyclerAdatper(val destModel: UserModel) : RecyclerView.Adapter<ChatRecyclerAdatper.ChatViewHolder>() {
+class ChatRecyclerAdatper(val destModel: UserModel) : RecyclerView.Adapter<ChatRecyclerAdatper.BaseViewHolder>() {
     var chatList = ArrayList<ChatModel.Comment>()
+    val TYPE_ME = 0
+    val TYPE_OTHER = 1
 
-    inner class ChatViewHolder(val binding: ChatItemLayoutBinding): RecyclerView.ViewHolder(binding.root) {
-        fun bindMyChat(position: Int) {
-            val constraintSet = ConstraintSet()
-            constraintSet.clone(binding.constraintLayout)
+    abstract class BaseViewHolder(binding: ViewDataBinding): RecyclerView.ViewHolder(binding.root) {
+        abstract fun bind(position: Int)
+    }
+
+    inner class MyChatViewHolder(val binding: ChatMeItemLayoutBinding): BaseViewHolder(binding) {
+        override fun bind(position: Int) {
             binding.comment = chatList[position]
-            binding.profileImageView.visibility = View.GONE
-            binding.nicknameTextView.visibility = View.GONE
-            binding.otherTimestampTextView.visibility = View.GONE
             binding.commentTextView.setBackgroundResource(R.drawable.chat_bubble_me)
         }
-        fun bindOtherChat(position: Int) {
+    }
+
+    inner class OtherChatViewHolder(val binding: ChatOtherItemLayoutBinding): BaseViewHolder(binding) {
+        override fun bind(position: Int) {
             binding.comment = chatList[position]
             binding.destModel = destModel
-            binding.myTimestampTextView.visibility = View.GONE
             binding.commentTextView.setBackgroundResource(R.drawable.chat_bubble_other)
             // 상대방이 프로필을 설정한 경우에만 불러오기
             if (destModel.profileImageUrl != null) {
@@ -40,16 +43,25 @@ class ChatRecyclerAdatper(val destModel: UserModel) : RecyclerView.Adapter<ChatR
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatViewHolder {
-        return ChatViewHolder(ChatItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+        return when(viewType) {
+            TYPE_ME -> MyChatViewHolder(ChatMeItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+            TYPE_OTHER -> OtherChatViewHolder(ChatOtherItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
     }
 
-    override fun onBindViewHolder(holder: ChatViewHolder, position: Int) {
-        if(chatList[position].uid == Firebase.auth.uid) { // 본인이라면
-            holder.bindMyChat(position)
-        } else { // 상대방이라면
-            holder.bindOtherChat(position)
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+        when(holder) {
+            is MyChatViewHolder -> holder.bind(position)
+            is OtherChatViewHolder -> holder.bind(position)
         }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if (chatList[position].uid == Firebase.auth.uid) return TYPE_ME
+        return TYPE_OTHER
     }
 
     override fun getItemCount(): Int {
