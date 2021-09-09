9ceb8ca8669d7c1823536ebfbb2a1e3377db0071
tmdgh1592 <tmdgh1592@naver.com>
2021-09-09 12:01:38 +0900

    - 카카오 공유하기 오류(lateinit not initialized) 발생 - 수정 완료 ㄴ GlobalApplication.kt -> KakaoSdk.init() 호출을 하지 않아서 발생했던 문제

--- app/src/main/java/com/app/buna/sharingmarket/koin/application/GlobalApplication.kt
+++ app/src/main/java/com/app/buna/sharingmarket/koin/application/GlobalApplication.kt
@@ -1,7 +1,9 @@
 package com.app.buna.sharingmarket.koin.application
 
 import android.app.Application
+import com.app.buna.sharingmarket.R
 import com.app.buna.sharingmarket.koin.myModule
+import com.kakao.sdk.common.KakaoSdk
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -12,7 +14,7 @@ class GlobalApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-//        KakaoSdk.init(this, getString(R.string.kakao_app_key))
+        KakaoSdk.init(this, getString(R.string.kakao_app_key))
 
         /*Koin*/
         startKoin {
